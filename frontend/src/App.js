bed73b30e23c501a44f782e254f3c425f4a3845a
regyap <100791182+regyap@users.noreply.github.com>
2022-09-13 23:49:22 +0800

    Add files via upload

---
+++ frontend/src/App.js
@@ -0,0 +1,27 @@
+import React, { useState } from 'react';
+import './App.css';
+import Huh from './IndexPage/FrontPage.js';
+import 'antd/dist/antd.css';
+// import { DownloadOutlined } from '@ant-design/icons';
+// import { Button, Radio } from 'antd';
+// import { TimePicker } from 'antd';
+
+
+
+function App() {
+  // const [size, setSize] = useState('large');
+  return(
+    <>
+    <body id="body">
+   
+
+ 
+    <Huh/>
+    
+
+
+    </body>
+    </>
+  )
+}
+export default App;
---
+++ frontend/src/IndexPage/button.js
@@ -0,0 +1,19 @@
+import React, {useState, useEffect} from "react";
+import './button.css';
+import Button from 'react-bootstrap/Button';
+
+
+
+
+function button() {
+   
+return(
+    <>
+    <Button variant="dark">Dark</Button>
+    <Button variant="link">Link</Button>
+    </>
+    
+)
+}
+
+export default button;
