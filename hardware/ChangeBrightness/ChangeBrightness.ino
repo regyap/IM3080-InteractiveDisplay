3d45b49169a8437bfcdd76b574ed7d1f7bf8ade2
Bing Hong <binghong.lim@gmail.com>
2022-10-18 23:01:07 +0800

    Added Random beats by brightness, clean fs

---
+++ hardware/ChangeBrightness/ChangeBrightness.ino
@@ -0,0 +1,534 @@
+#include <Adafruit_NeoPixel.h>
+#ifdef __AVR__
+#include <avr/power.h> // Required for 16 MHz Adafruit Trinket
+#endif
+
+//LED pins [FILL IN THE PIN NO!!]
+#define LED_PIN1 
+#define LED_PIN2
+#define LED_PIN3
+#define LED_PIN4
+#define LED_PIN5
+#define LED_PIN6
+#define LED_PIN7
+#define LED_PIN8
+#define LED_PIN9
+#define LED_PIN10 
+#define LED_PIN11
+#define LED_PIN12
+#define LED_PIN13
+#define LED_PIN14
+#define LED_PIN15
+#define LED_PIN16
+#define LED_PIN17
+#define LED_PIN18
+#define LED_PIN19 
+#define LED_PIN20
+#define LED_PIN21
+#define LED_PIN22
+#define LED_PIN23
+#define LED_PIN24
+#define LED_PIN25
+int ledPin[25] = {};
+
+
+#define LED_COUNT 20
+
+//Ultrasonic sensor pins [FILL IN PIN NO!!!]
+#define trigPin1 
+#define trigPin2 
+#define trigPin3 
+#define trigPin4 
+#define trigPin5 
+#define trigPin6
+#define echoPin1 
+#define echoPin2 
+#define echoPin3 
+#define echoPin4 
+#define echoPin5 
+#define echoPin6 
+
+Adafruit_NeoPixel strip(LED_COUNT, ledPin[0], NEO_GRB + NEO_KHZ800);
+
+int brightnessLow = 150;
+int brightnessHigh = 255;
+int wait = 1000;
+
+//for sensors
+int maxDistance = 10;
+int triggeredNo = 0;
+
+//For multithreading
+long lastTime = millis();
+
+void setup() {
+  strip.begin();
+  strip.show();
+  strip.setBrightness(brightnessLow);
+
+  Serial.begin(9600);
+  pinMode(echoPin1, INPUT);
+  pinMode(echoPin2, INPUT);
+  pinMode(echoPin3, INPUT);
+  pinMode(echoPin4, INPUT);
+  pinMode(echoPin5, INPUT);
+  pinMode(echoPin6, INPUT);
+  pinMode(trigPin1, OUTPUT);
+  pinMode(trigPin2, OUTPUT);
+  pinMode(trigPin3, OUTPUT);
+  pinMode(trigPin4, OUTPUT);
+  pinMode(trigPin5, OUTPUT);
+  pinMode(trigPin6, OUTPUT);
+}
+
+
+// Determine if a sensor is triggered
+bool SensorIsTriggered(int trigPin, int echoPin, long elapsedMS) {
+  digitalWrite(trigPin, LOW);
+
+  static long sensorTime = 0;
+  sensorTime += elapsedMS;
+  if (sensorTime >= 2 && sensorTime < 12) {
+    digitalWrite(trigPin, HIGH);
+  }
+  if (sensorTime >= 12) {
+    digitalWrite(trigPin, LOW);
+  }
+  
+  unsigned duration = pulseIn(echoPin, HIGH);
+  int distance = duration * 0.034 / 2;
+  if (distance <= maxDistance) {
+    triggeredNo += 1;
+    return true;
+  } else {
+    return false;
+  }
+}
+
+void ChangeBrightness(int pin, long elapsedMS) {
+  strip.setPin(pin);
+  strip.setBrightness(brightnessHigh);
+  strip.show();
+
+  static long ledTime = 0;
+  ledTime += elapsedMS;
+  if (ledTime >= wait) {
+    strip.setBrightness(brightnessLow);
+    strip.show();
+    ledTime -= wait;
+  }
+}
+
+
+void CenterRipple(long elapsedTime) {
+  static long ledTime = 0;
+  ledTime += elapsedTime;
+
+  if (ledTime <= 1000) {
+    ChangeBrightness(ledPin[12], elapsedTime);
+  };
+  if (ledTime >= 1000 && ledTime < 2000) {
+    ChangeBrightness(ledPin[7], elapsedTime);
+    ChangeBrightness(ledPin[8], elapsedTime);
+    ChangeBrightness(ledPin[11], elapsedTime);
+    ChangeBrightness(ledPin[13], elapsedTime);
+    ChangeBrightness(ledPin[16], elapsedTime);
+    ChangeBrightness(ledPin[17], elapsedTime);
+  };
+  if (ledTime >= 2000 && ledTime < 3000) {
+    ChangeBrightness(ledPin[2], elapsedTime);
+    ChangeBrightness(ledPin[3], elapsedTime);
+    ChangeBrightness(ledPin[4], elapsedTime);
+    ChangeBrightness(ledPin[6], elapsedTime);
+    ChangeBrightness(ledPin[9], elapsedTime);
+    ChangeBrightness(ledPin[10], elapsedTime);
+    ChangeBrightness(ledPin[14], elapsedTime);
+    ChangeBrightness(ledPin[15], elapsedTime);
+    ChangeBrightness(ledPin[18], elapsedTime);
+    ChangeBrightness(ledPin[20], elapsedTime);
+    ChangeBrightness(ledPin[21], elapsedTime);
+    ChangeBrightness(ledPin[22], elapsedTime);
+  };
+  if (ledTime >= 3000 && ledTime < 4000) {
+    ChangeBrightness(ledPin[0], elapsedTime);
+    ChangeBrightness(ledPin[1], elapsedTime);
+    ChangeBrightness(ledPin[5], elapsedTime);
+    ChangeBrightness(ledPin[19], elapsedTime);
+    ChangeBrightness(ledPin[23], elapsedTime);
+    ChangeBrightness(ledPin[24], elapsedTime);
+  };
+  if (ledTime >= 4000) {
+    ledTime -= 4000;
+  }
+}
+
+void DirectionRipple1(long elapsedTime) {
+  static long ledTime = 0;
+  ledTime += elapsedTime;
+
+  if (ledTime <= 1000) {
+    ChangeBrightness(ledPin[0], elapsedTime);
+  };
+  if (ledTime >= 1000 && ledTime < 2000) {
+    ChangeBrightness(ledPin[1], elapsedTime);
+    ChangeBrightness(ledPin[2], elapsedTime);
+    ChangeBrightness(ledPin[3], elapsedTime);
+    ChangeBrightness(ledPin[4], elapsedTime);
+    ChangeBrightness(ledPin[5], elapsedTime);
+  };
+  if (ledTime >= 2000 && ledTime < 3000) {
+    ChangeBrightness(ledPin[6], elapsedTime);
+    ChangeBrightness(ledPin[7], elapsedTime);
+    ChangeBrightness(ledPin[8], elapsedTime);
+    ChangeBrightness(ledPin[9], elapsedTime);
+  };
+  if (ledTime >= 2000) {
+    ledTime -= 2000;
+  }
+  if (ledTime >= 3000 && ledTime < 4000) {
+    ChangeBrightness(ledPin[10], elapsedTime);
+    ChangeBrightness(ledPin[11], elapsedTime);
+    ChangeBrightness(ledPin[12], elapsedTime);
+    ChangeBrightness(ledPin[13], elapsedTime);
+    ChangeBrightness(ledPin[14], elapsedTime);
+  };
+  if (ledTime >= 4000 && ledTime < 5000) {
+    ChangeBrightness(ledPin[15], elapsedTime);
+    ChangeBrightness(ledPin[16], elapsedTime);
+    ChangeBrightness(ledPin[17], elapsedTime);
+    ChangeBrightness(ledPin[18], elapsedTime);
+  };
+  if (ledTime >= 5000 && ledTime < 6000) {
+    ChangeBrightness(ledPin[19], elapsedTime);
+    ChangeBrightness(ledPin[20], elapsedTime);
+    ChangeBrightness(ledPin[21], elapsedTime);
+    ChangeBrightness(ledPin[22], elapsedTime);
+    ChangeBrightness(ledPin[23], elapsedTime);
+  };  
+  if (ledTime >= 5000 && ledTime < 6000) {
+    ChangeBrightness(ledPin[24], elapsedTime);
+  };
+  if (ledTime >= 6000) {
+    ledTime -= 6000;
+  }
+}
+
+void DirectionRipple2(long elapsedTime) {
+  static long ledTime = 0;
+  ledTime += elapsedTime;
+  if (ledTime <= 1000) {
+    ChangeBrightness(ledPin[1], elapsedTime);
+  };
+  if (ledTime >= 1000 && ledTime < 2000) {
+    ChangeBrightness(ledPin[0], elapsedTime);
+    ChangeBrightness(ledPin[2], elapsedTime);
+    ChangeBrightness(ledPin[6], elapsedTime);
+    ChangeBrightness(ledPin[10], elapsedTime);
+  };
+  if (ledTime >= 2000 && ledTime < 3000) {
+    ChangeBrightness(ledPin[3], elapsedTime);
+    ChangeBrightness(ledPin[7], elapsedTime);
+    ChangeBrightness(ledPin[11], elapsedTime);
+    ChangeBrightness(ledPin[15], elapsedTime);
+    ChangeBrightness(ledPin[19], elapsedTime);
+  };
+  if (ledTime >= 3000 && ledTime < 4000) {
+    ChangeBrightness(ledPin[4], elapsedTime);
+    ChangeBrightness(ledPin[8], elapsedTime);
+    ChangeBrightness(ledPin[12], elapsedTime);
+    ChangeBrightness(ledPin[16], elapsedTime);
+    ChangeBrightness(ledPin[20], elapsedTime);
+  };
+  if (ledTime >= 4000 && ledTime < 5000) {
+    ChangeBrightness(ledPin[5], elapsedTime);
+    ChangeBrightness(ledPin[9], elapsedTime);
+    ChangeBrightness(ledPin[13], elapsedTime);
+    ChangeBrightness(ledPin[17], elapsedTime);
+    ChangeBrightness(ledPin[21], elapsedTime);
+  };
+  if (ledTime >= 5000 && ledTime < 6000) {
+    ChangeBrightness(ledPin[14], elapsedTime);
+    ChangeBrightness(ledPin[18], elapsedTime);
+    ChangeBrightness(ledPin[22], elapsedTime);
+    ChangeBrightness(ledPin[24], elapsedTime);
+  };  
+  if (ledTime >= 5000 && ledTime < 6000) {
+    ChangeBrightness(ledPin[23], elapsedTime);
+  };
+  if (ledTime >= 6000) {
+    ledTime -= 6000;
+  }
+}
+
+void DirectionRipple3(long elapsedTime) {
+  static long ledTime = 0;
+  ledTime += elapsedTime;
+
+  if (ledTime <= 1000) {
+    ChangeBrightness(ledPin[5], elapsedTime);
+  };
+  if (ledTime >= 1000 && ledTime < 2000) {
+    ChangeBrightness(ledPin[0], elapsedTime);
+    ChangeBrightness(ledPin[4], elapsedTime);
+    ChangeBrightness(ledPin[9], elapsedTime);
+    ChangeBrightness(ledPin[14], elapsedTime);
+  };
+  if (ledTime >= 2000 && ledTime < 3000) {
+    ChangeBrightness(ledPin[3], elapsedTime);
+    ChangeBrightness(ledPin[8], elapsedTime);
+    ChangeBrightness(ledPin[13], elapsedTime);
+    ChangeBrightness(ledPin[18], elapsedTime);
+    ChangeBrightness(ledPin[23], elapsedTime);
+  };
+  if (ledTime >= 3000 && ledTime < 4000) {
+    ChangeBrightness(ledPin[2], elapsedTime);
+    ChangeBrightness(ledPin[7], elapsedTime);
+    ChangeBrightness(ledPin[12], elapsedTime);
+    ChangeBrightness(ledPin[17], elapsedTime);
+    ChangeBrightness(ledPin[22], elapsedTime);
+  };
+  if (ledTime >= 4000 && ledTime < 5000) {
+    ChangeBrightness(ledPin[1], elapsedTime);
+    ChangeBrightness(ledPin[6], elapsedTime);
+    ChangeBrightness(ledPin[11], elapsedTime);
+    ChangeBrightness(ledPin[16], elapsedTime);
+    ChangeBrightness(ledPin[21], elapsedTime);
+  };
+  if (ledTime >= 5000 && ledTime < 6000) {
+    ChangeBrightness(ledPin[10], elapsedTime);
+    ChangeBrightness(ledPin[15], elapsedTime);
+    ChangeBrightness(ledPin[20], elapsedTime);
+    ChangeBrightness(ledPin[24], elapsedTime);
+  };  
+  if (ledTime >= 5000 && ledTime < 6000) {
+    ChangeBrightness(ledPin[19], elapsedTime);
+  };
+  if (ledTime >= 6000) {
+    ledTime -= 6000;
+  }
+}
+
+void DirectionRipple4(long elapsedTime) {
+  static long ledTime = 0;
+  ledTime += elapsedTime;
+
+  if (ledTime <= 1000) {
+    ChangeBrightness(ledPin[19], elapsedTime);
+  };
+  if (ledTime >= 1000 && ledTime < 2000) {
+    ChangeBrightness(ledPin[10], elapsedTime);
+    ChangeBrightness(ledPin[15], elapsedTime);
+    ChangeBrightness(ledPin[20], elapsedTime);
+    ChangeBrightness(ledPin[24], elapsedTime);
+  };
+  if (ledTime >= 2000 && ledTime < 3000) {
+    ChangeBrightness(ledPin[1], elapsedTime);
+    ChangeBrightness(ledPin[6], elapsedTime);
+    ChangeBrightness(ledPin[11], elapsedTime);
+    ChangeBrightness(ledPin[16], elapsedTime);
+    ChangeBrightness(ledPin[21], elapsedTime);
+  };
+  if (ledTime >= 3000 && ledTime < 4000) {
+    ChangeBrightness(ledPin[2], elapsedTime);
+    ChangeBrightness(ledPin[7], elapsedTime);
+    ChangeBrightness(ledPin[12], elapsedTime);
+    ChangeBrightness(ledPin[17], elapsedTime);
+    ChangeBrightness(ledPin[22], elapsedTime);
+  };
+  if (ledTime >= 4000 && ledTime < 5000) {
+    ChangeBrightness(ledPin[3], elapsedTime);
+    ChangeBrightness(ledPin[8], elapsedTime);
+    ChangeBrightness(ledPin[13], elapsedTime);
+    ChangeBrightness(ledPin[18], elapsedTime);
+    ChangeBrightness(ledPin[23], elapsedTime);
+  };
+  if (ledTime >= 5000 && ledTime < 6000) {
+    ChangeBrightness(ledPin[0], elapsedTime);
+    ChangeBrightness(ledPin[4], elapsedTime);
+    ChangeBrightness(ledPin[9], elapsedTime);
+    ChangeBrightness(ledPin[14], elapsedTime);
+  };  
+  if (ledTime >= 5000 && ledTime < 6000) {
+    ChangeBrightness(ledPin[5], elapsedTime);
+  };
+  if (ledTime >= 6000) {
+    ledTime -= 6000;
+  }
+}
+
+void DirectionRipple5(long elapsedTime) {
+  static long ledTime = 0;
+  ledTime += elapsedTime;
+  if (ledTime <= 1000) {
+    ChangeBrightness(ledPin[23], elapsedTime);
+  };
+  if (ledTime >= 1000 && ledTime < 2000) {
+    ChangeBrightness(ledPin[14], elapsedTime);
+    ChangeBrightness(ledPin[18], elapsedTime);
+    ChangeBrightness(ledPin[22], elapsedTime);
+    ChangeBrightness(ledPin[24], elapsedTime);
+  };
+  if (ledTime >= 2000 && ledTime < 3000) {
+    ChangeBrightness(ledPin[5], elapsedTime);
+    ChangeBrightness(ledPin[9], elapsedTime);
+    ChangeBrightness(ledPin[13], elapsedTime);
+    ChangeBrightness(ledPin[17], elapsedTime);
+    ChangeBrightness(ledPin[21], elapsedTime);
+  };
+  if (ledTime >= 3000 && ledTime < 4000) {
+    ChangeBrightness(ledPin[4], elapsedTime);
+    ChangeBrightness(ledPin[8], elapsedTime);
+    ChangeBrightness(ledPin[12], elapsedTime);
+    ChangeBrightness(ledPin[16], elapsedTime);
+    ChangeBrightness(ledPin[20], elapsedTime);
+  };
+  if (ledTime >= 4000 && ledTime < 5000) {
+    ChangeBrightness(ledPin[3], elapsedTime);
+    ChangeBrightness(ledPin[7], elapsedTime);
+    ChangeBrightness(ledPin[11], elapsedTime);
+    ChangeBrightness(ledPin[15], elapsedTime);
+    ChangeBrightness(ledPin[19], elapsedTime);
+  };
+  if (ledTime >= 5000 && ledTime < 6000) {
+    ChangeBrightness(ledPin[0], elapsedTime);
+    ChangeBrightness(ledPin[2], elapsedTime);
+    ChangeBrightness(ledPin[6], elapsedTime);
+    ChangeBrightness(ledPin[10], elapsedTime);
+  };  
+  if (ledTime >= 5000 && ledTime < 6000) {
+    ChangeBrightness(ledPin[1], elapsedTime);
+  };
+  if (ledTime >= 6000) {
+    ledTime -= 6000;
+  }
+}
+
+void DirectionRipple6(long elapsedTime) {
+  static long ledTime = 0;
+  ledTime += elapsedTime;
+
+  if (ledTime <= 1000) {
+    ChangeBrightness(ledPin[24], elapsedTime);
+  };
+  if (ledTime >= 1000 && ledTime < 2000) {
+    ChangeBrightness(ledPin[19], elapsedTime);
+    ChangeBrightness(ledPin[20], elapsedTime);
+    ChangeBrightness(ledPin[21], elapsedTime);
+    ChangeBrightness(ledPin[22], elapsedTime);
+    ChangeBrightness(ledPin[23], elapsedTime);
+  };
+  if (ledTime >= 2000 && ledTime < 3000) {
+    ChangeBrightness(ledPin[15], elapsedTime);
+    ChangeBrightness(ledPin[16], elapsedTime);
+    ChangeBrightness(ledPin[17], elapsedTime);
+    ChangeBrightness(ledPin[18], elapsedTime);    
+  };
+  if (ledTime >= 3000 && ledTime < 4000) {
+    ChangeBrightness(ledPin[10], elapsedTime);
+    ChangeBrightness(ledPin[11], elapsedTime);
+    ChangeBrightness(ledPin[12], elapsedTime);
+    ChangeBrightness(ledPin[13], elapsedTime);
+    ChangeBrightness(ledPin[14], elapsedTime);
+  };
+  if (ledTime >= 4000 && ledTime < 5000) {
+    ChangeBrightness(ledPin[6], elapsedTime);
+    ChangeBrightness(ledPin[7], elapsedTime);
+    ChangeBrightness(ledPin[8], elapsedTime);
+    ChangeBrightness(ledPin[9], elapsedTime);
+  };
+  if (ledTime >= 5000 && ledTime < 6000) {
+    ChangeBrightness(ledPin[1], elapsedTime);
+    ChangeBrightness(ledPin[2], elapsedTime);
+    ChangeBrightness(ledPin[3], elapsedTime);
+    ChangeBrightness(ledPin[4], elapsedTime);
+    ChangeBrightness(ledPin[5], elapsedTime);
+  };  
+  if (ledTime >= 5000 && ledTime < 6000) {
+    ChangeBrightness(ledPin[0], elapsedTime);
+  };
+  if (ledTime >= 6000) {
+    ledTime -= 6000;
+  }
+}
+
+void rainbow(long elapsedTime) {      //wait = 20
+  static int i = 0;
+  static int j = 0 ;
+  static long RainbowTime = 0;
+  RainbowTime += elapsedTime;
+
+  if (RainbowTime >= 100) {
+    for (int n=0; n<25; n++) {
+      strip.setPin(ledPin[n]);
+      if (i <= strip.numPixels()) {
+       i++;
+      }
+      else if (i >= strip.numPixels()) {
+        j++;
+      }
+      if (i + j >= 256) {
+        i = 0;
+        j = 0;
+     }
+      strip.fill(Wheel((i + j)));
+    //    Serial.println(String(i + j));
+     strip.show();
+    }
+    RainbowTime -= 100;
+  }
+}
+
+uint32_t Wheel(byte WheelPos) {
+  WheelPos = 255 - WheelPos;
+  if (WheelPos < 85) {
+    //    Serial.println("red to blue -" + String(WheelPos * 3));
+    return strip.Color(255 - WheelPos * 3, 0, WheelPos * 3);
+  }
+  if (WheelPos < 170) {
+    WheelPos -= 85;
+    //    Serial.println("blue to green =" + String(WheelPos * 3));
+    return strip.Color(0, WheelPos * 3, 255 - WheelPos * 3);
+  }
+  WheelPos -= 170;
+  //  Serial.println("green to red _" + String(WheelPos * 3));
+  return strip.Color(WheelPos * 3, 255 - WheelPos * 3, 0);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  long elapsedTime = millis() - lastTime; //get time elapsed
+  lastTime += elapsedTime;
+
+  bool sensor1Triggered = SensorIsTriggered(trigPin1, echoPin1, elapsedTime);
+  bool sensor2Triggered = SensorIsTriggered(trigPin2, echoPin2, elapsedTime);
+  bool sensor3Triggered = SensorIsTriggered(trigPin3, echoPin3, elapsedTime);
+  bool sensor4Triggered = SensorIsTriggered(trigPin4, echoPin4, elapsedTime);
+  bool sensor5Triggered = SensorIsTriggered(trigPin5, echoPin5, elapsedTime);
+  bool sensor6Triggered = SensorIsTriggered(trigPin6, echoPin6, elapsedTime);
+
+  if (triggeredNo > 1) {
+    CenterRipple(elapsedTime);
+  }else if (triggeredNo == 1){
+    if (sensor1Triggered){
+      DirectionRipple1(elapsedTime);
+    }
+    if (sensor2Triggered){
+      DirectionRipple2(elapsedTime);
+    }
+    if (sensor3Triggered){
+      DirectionRipple3(elapsedTime);
+    }
+    if (sensor4Triggered){
+      DirectionRipple4(elapsedTime);
+    }
+    if (sensor5Triggered){
+      DirectionRipple5(elapsedTime);
+    }
+    if (sensor6Triggered){
+      DirectionRipple6(elapsedTime);
+    }
+  }
+  rainbow(20);
+
+  triggeredNo = 0;
+}
---
+++ hardware/RandoBeats/RandoBeats.ino
@@ -0,0 +1,203 @@
+#include <Adafruit_NeoPixel.h>
+#ifdef __AVR__
+#include <avr/power.h> // Required for 16 MHz Adafruit Trinket
+#endif
+
+long beat[] = {17, 484, 810, 2683, 4180, 4738, 6003, 6359, 6721, 7952, 8488,
+               8809, 10204, 10698, 11459, 13676, 14699, 15996, 16768, 18746,
+               20710, 22732, 23948, 24740, 26720, 27457, 29703, 30743, 31993,
+               32771, 34720, 36717, 38087, 38702, 40486, 40796, 42714, 43461,
+               44281, 45696, 46721, 47996, 48778, 50719, 52715, 54094, 54729,
+               55946, 56719, 58734, 59457, 60227, 61719, 63985, 64789, 66706,
+               68703, 70102, 70739, 71958, 72726, 74709, 75455, 76235, 77706,
+               80682, 82678, 84685, 86694, 88692, 90674, 91495, 92213, 93699,
+               94738, 95745, 96477, 96780, 97082, 98677, 100680, 102682, 103943,
+               104470, 106687, 107498, 108237, 109702, 110692, 112674, 113444,
+               114672, 116679, 117460, 118699, 119945, 120696, 121470, 122671,
+               124225, 124695, 125463, 126687, 127979, 128670, 129438, 130667,
+               132689, 133460, 34685, 135948, 136674, 137471, 138670, 140246,
+               140685, 141458, 142686, 143996, 144682, 145456, 146672, 148687,
+               149472, 150693, 152704, 154682
+              };
+
+long beat1[] = {15, 728, 1459, 2207, 2966, 3671, 4467, 4850, 5217, 5973, 6679,
+                7489, 7861, 8968, 9672, 10463, 10850, 11954, 12664, 13490, 13850,
+                14233, 14966, 15656, 16474, 16856, 17979, 18649, 19483, 19844,
+                20218, 20978, 21673, 22469, 22854, 23221, 23978, 24659, 25475,
+                25859, 26956, 27657, 28480, 28829, 29229, 29979, 30649, 31464,
+                31844, 32239, 32967, 33668, 34480, 34843, 35982, 36671, 37489,
+                37848, 38211, 38980, 39638, 40467, 40855, 41964, 42656, 43476,
+                43853, 44229, 44966, 45674, 46479, 46837, 47222, 47982, 48655,
+                49482, 49844, 51865, 52523, 53230, 54755, 55294, 55952, 56688,
+                57791, 58151, 58512, 59201, 60748, 61283, 61983, 63903, 65244,
+                66738, 67302, 67969, 68654, 69748, 70168, 71234, 72730, 73285,
+                73950, 74964, 75669, 76469, 76852, 77209, 77976, 78664, 79451,
+                79849, 80970, 81662, 82501, 82838, 83963, 84682, 85476, 85833,
+                86219, 86973, 87650, 88457, 88842, 89966, 90673, 91472, 91855,
+                92210, 92966, 93678, 94481, 94853, 95222, 95973, 96654, 97477,
+                97842, 98964, 99660, 100468, 100852, 101232, 101971, 102655,
+                103456, 103843, 104983, 105661, 106467, 106865, 107967, 108670,
+                109476, 109847, 110216, 110962, 111648, 112486, 112848, 113982,
+                114658, 115484, 115868, 116220, 116961, 117666, 118483, 118829,
+                119981, 120657, 121483, 121846, 123889, 124506, 125253, 126768,
+                127300, 127948, 129872
+               };
+
+int LED_COUNT = 60;
+unsigned long lastTime = 0;
+int brightness = 200;
+int ledPinMin = 22;
+int ledPinMax = 30;
+int duration = 1000;
+
+Adafruit_NeoPixel strip(LED_COUNT, ledPinMin, NEO_GRB + NEO_KHZ800);
+
+void setup() {
+  strip.begin();
+  strip.show();
+  strip.setBrightness(brightness);
+  Serial.begin(9600);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  long elapsedTime = millis() - lastTime;
+  lastTime = lastTime + elapsedTime;
+
+  raise(elapsedTime, beat);
+}
+
+////////////////////////////
+//  | 3 | 4 | 5 |   |   |
+//    ^           ^
+//    |           |
+//  popNum      pushNum
+////////////////////////////
+void raise(long elapsedTime, long beat[]) {
+  static long ledsTime[25]; //Keep track of all the LED times
+  static long queue[25]; //Queue to keep track of what tube is still doing patterns
+  static int popNum = 0; //Variable to deal with Queue implementation
+  static int pushNum = 0; //Variable to deal with Queue implementation
+  static int beatCounter = 0; //Track which beat is it on
+  static long functionTime = 0; //Track how long this function has been running
+
+  functionTime = functionTime + elapsedTime;
+
+  if (functionTime > beat[beatCounter]) {
+    queue[pushNum] = 0;
+
+    //Managing random number
+    int randNumber = random(ledPinMin, ledPinMax); //Choose random tube to light up
+    while (true) {  //Checking for duplicate rand num
+      if (pushNum > popNum) { //Handle normal case
+        for (int j = popNum; j < pushNum; j++) {
+          if (queue[j] == randNumber) {
+            randNumber = random(ledPinMin, ledPinMax)
+            continue;
+          } else {
+            break;
+          }
+        }
+      } else if (pushNum < popNum) { //Handle Circular Buffer situation
+        for (int j = popNum; j <= 25; j++) {
+          if (queue[j] == randNumber) {
+            randNumber = random(ledPinMin, ledPinMax)
+            continue;
+          } else {
+            break;
+          }
+        }
+        for (int j = 0; j < pushNum; j++) {
+          if (queue[j] == randNumber) {
+            randNumber = random(ledPinMin, ledPinMax)
+            continue;
+          } else {
+            break;
+          }
+        }
+      }
+    }
+
+    //Pushing Random number to queue
+    queue[pushNum] = randNumber;  //Add the tube number to queue to keep track
+    strip.setPin(randNumber);
+    strip.fill(strip.Color(255,255,255));
+    strip.setBrightness(0);
+    
+    //Update push variable
+    if (pushNum == 25) {
+      pushNum = 0;
+    } else {
+      pushNum++;
+    }
+    //Update beats variable
+    beatCounter++;
+  } else if (pushNum == popNum) { //When theres no tube running pattern, end function
+    return;
+  }
+
+
+  //Else, manage tube still in pattern
+  if (pushNum > popNum) { //Handle normal case
+    for (int j = popNum; j < pushNum; j++) {
+      int tube = queue[j];
+      strip.setPin(tube);
+      ledsTime[tube] += elapsedTime;
+      int tubeTime = ledsTime[tube];
+      if (tubeTime > duration) { //if the tube exceeded 1000ms(the duration)
+        ledsTime[tube] = 0;
+        strip.clear();
+        strip.show();
+        if (popNum == 25) { //Update pop variable (due to circular)
+          popNum = 0;
+        } else {
+          popNum++;
+        }
+      } else {  //else, change brightness of tube in pattern
+        strip.setBrightness((tubeTime / duration * 255));
+        strip.show();
+      }
+    }
+  } else if (pushNum < popNum) { //Handle Circular Buffer situation
+    for (int j = popNum; j <= 25; j++) {
+      int tube = queue[j];
+      strip.setPin(tube);
+      ledsTime[tube] += elapsedTime;
+      int tubeTime = ledsTime[tube];
+      if (tubeTime > duration) { //if the tube exceeded 1000ms(the duration)
+        ledsTime[tube] = 0;
+        strip.clear();
+        strip.show();
+        if (popNum == 25) { //Update pop variable (due to circular)
+          popNum = 0;
+        } else {
+          popNum++;
+        }
+      } else {  //else, change brightness of tube in pattern
+        strip.setBrightness((tubeTime / duration * 255));
+        strip.show();
+      }
+    }
+
+    for (int j = 0; j < pushNum; j++) {
+      int tube = queue[j];
+      strip.setPin(tube);
+      ledsTime[tube] += elapsedTime;
+      int tubeTime = ledsTime[tube];
+      if (tubeTime > duration) { //if the tube exceeded 1000ms(the duration)
+        ledsTime[tube] = 0;
+        strip.clear();
+        strip.show();
+        if (popNum == 25) { //Update pop variable (due to circular)
+          popNum = 0;
+        } else {
+          popNum++;
+        }
+      } else {  //else, change brightness of tube in pattern
+        strip.setBrightness((tubeTime / duration * 255));
+        strip.show();
+      }
+    }
+  }
+
+}
