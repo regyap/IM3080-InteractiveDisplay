4c4278e62ff2cfdcf4a534636be293ee265d4ba4
regyap <regineyap1609@gmail.com>
2022-11-09 21:31:48 +0800

    mobile add page

---
+++ frontend/src/Interactive/Interactive.js
@@ -0,0 +1,15 @@
+import React, { useState, useEffect } from "react";
+import { useNavigate } from "react-router-dom";
+import socketOBJ from "../Socket/SocketIO.js";
+import Logo from "../assets/ntu2.png";
+import "./Interactive.css";
+
+function Interactive() {
+  
+
+  return (
+   <>
+   </>
+  );
+}
+export default Interactive;
